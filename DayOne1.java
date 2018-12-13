8ccb9daa36e12cacfa54b2c9c140c8f63fbe58d2
Frida Mattsson <34581085+mattssonf@users.noreply.github.com>
2018-12-13 19:44:53 +0100

    New version
    Added solution to the second part fo day 1 and changed the layout of the code

--- DayOne1.java
+++ DayOne1.java
@@ -1,9 +1,11 @@
 import java.io.*;
+import java.util.Set;
+import java.util.TreeSet;
 
-public class DayOne1 {
-    public static void main(String args[]) throws Exception{
-        File frequencies = new File("frequencies.txt");
-        BufferedReader br = new BufferedReader(new FileReader(frequencies));
+public class DayOne {
+
+    public static void AddFromFile(File file) throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
         String line;
         int sum = 0;
@@ -19,5 +21,44 @@ public class DayOne1 {
             }
         }
         System.out.println("The sum of the frequencies is: " + sum);
+    }
+
+    public static void FindDuplicateFrequency(File file) throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        Set<Integer> sums = new TreeSet<Integer>();
+
+        String line;
+        int sum = 0;
+        String number;
+        br.mark(5000);
+        while(true){
+            line = br.readLine();
+            if(line  == null){
+                br.reset();
+                line = br.readLine();
+            }
+            if(line.charAt(0) == '-') {
+                number = line.substring(1);
+                sum = sum - Integer.parseInt(number);
+                if(sums.contains(sum))
+                    break;
+                sums.add(sum);
+            }
+            else {
+                number = line.substring(1);
+                sum = sum + Integer.parseInt(number);
+                if(sums.contains(sum))
+                    break;
+                sums.add(sum);
+            }
+            System.out.println(line);
+        }
+        System.out.println("The first duplicate frequency is: " + sum);
+    }
+
+    public static void main(String args[]) throws Exception{
+        File frequencies = new File("frequencies.txt");
+        AddFromFile(frequencies);
+        FindDuplicateFrequency(frequencies);
     }
 }
